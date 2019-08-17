21587a84b2296ac4d21f8119033161039a9e25ea
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-17 00:19:22 -0400

    Added basic Visual Studio Solution

---
+++ LemonMissionPack/Script.cs
@@ -0,0 +1,13 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonMissionPack
+{
+    public class LemonMissionPack : Script
+    {
+    }
+}
