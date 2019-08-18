6744da1265a8dc9c9d65ffd2ac4df96c2a10598c
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-18 02:32:20 -0400

    Added basic support for user progress

---
+++ LemonMissionPack/UserProgress.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace LemonMissionPack
+{
+    /// <summary>
+    /// The mission identifiers.
+    /// </summary>
+    public enum Mission
+    {
+        None = -1
+    }
+
+    public class UserProgress
+    {
+        /// <summary>
+        /// The last completed mission by the player.
+        /// </summary>
+        [JsonProperty("last_mission")]
+        public Mission LastMission { get; set; }
+    }
+}
