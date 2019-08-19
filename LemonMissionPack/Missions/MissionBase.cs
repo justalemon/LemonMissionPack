b0f07da57f97f143091f7448c8e4a8f2b3032921
Hannele Ruiz <justlemoncl@gmail.com>
2019-08-19 00:00:52 -0400

    Don't make the mission base properties static
    I don't want race conditions between the scripts, and also I'm fucking stupid by doing that

--- LemonMissionPack/Missions/MissionBase.cs
+++ LemonMissionPack/Missions/MissionBase.cs
@@ -1,9 +1,4 @@
 ﻿using GTA;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LemonMissionPack.Missions
 {
@@ -12,14 +7,14 @@ namespace LemonMissionPack.Missions
         /// <summary>
         /// If the player has been notified about the mission.
         /// </summary>
-        public static bool IsPlayerNotified { get; set; } = false;
+        public bool IsPlayerNotified { get; set; } = false;
         /// <summary>
         /// If the mission is currently in progress.
         /// </summary>
-        public static bool IsInProgress { get; set; } = false;
+        public bool IsInProgress { get; set; } = false;
         /// <summary>
         /// The current blip used to mark mission objectives.
         /// </summary>
-        public static Blip MissionBlip { get; set; }
+        public Blip MissionBlip { get; set; }
     }
 }
